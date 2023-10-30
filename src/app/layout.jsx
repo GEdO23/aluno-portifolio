8512c45131dd20d6b1976772a1dc8011f68a31c1
GEdO23 <rm99632@fiap.com.br>
2023-10-30 08:57:25 -0300

    Testando proteção da branch main.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,6 +15,7 @@ export default function RootLayout({children}) {
   <html lang="pt-br" >
     <body className = {inter.className} > 
       {children}
+      <p>Teste</p>
     </body> 
   </html>
   )
